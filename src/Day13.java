70ae99add98fbbacdba82d39eca238c9e9848626
Syntx <marcdecker2004@gmail.com>
2023-12-15 22:15:33 +0100

    added day13 part 2

--- src/Day13.java
+++ src/Day13.java
@@ -11,23 +11,35 @@ public class Day13 {
 
 	public static void main(String[] args) {
 		partOne();
+		partTwo();
 	}
 	
 	static void partOne() {
 		String[][] patterns = parsePatterns();
 		long sum = 0;
-		
+		int smug = 0;
 		for (String[] pattern : patterns) {
-			long res = findReflection(pattern);
+			long res = findReflection(pattern, smug);
 			sum += res;
 		}
 		System.out.println(sum);
 	}
 	
-	static long findReflection(String[] pattern) {
-		long result = findVerticalReflection(pattern);
+	static void partTwo() {
+		String[][] patterns = parsePatterns();
+		long sum = 0;
+		int smug = 1;
+		for (String[] pattern : patterns) {
+			long res = findReflection(pattern, smug);
+			sum += res;
+		}
+		System.out.println(sum);
+	}
+	
+	static long findReflection(String[] pattern, int smug) {
+		long result = findVerticalReflection(pattern, smug);
 		if (result == -1) {
-			result = findHorizontalReflection(pattern);
+			result = findHorizontalReflection(pattern, smug);
 		}
 		if (result == -1) {
 			System.out.println("Pattern has not reflection");
@@ -35,48 +47,62 @@ public class Day13 {
 		return result;
 	}
 	
-	static long findVerticalReflection(String[] pattern) {
+	static long findVerticalReflection(String[] pattern, int smug) {
 		for (int i = 0; i < pattern[0].length() - 1; i++) {
-			if (isVerticalReflected(pattern, i)) {
+			if (isVerticalReflected(pattern, i, smug)) {
 				return i + 1;
 			}
 		}
 		return -1;
 	}
 	
-	static boolean isVerticalReflected(String[] pattern, int i) {
+	static boolean isVerticalReflected(String[] pattern, int i, int smug) {
 		int left = i;
 		int right = i + 1;
 		while (left >= 0 && right < pattern[0].length()) {
 			for (String line: pattern) {
 				if (line.charAt(left) != line.charAt(right)) {
-					return false;
+					smug--;
+					if (smug < 0) {
+						return false;
+					}
 				}
 			}
 			left--;
 			right++;
 		}
+		if (smug != 0) {
+			return false;
+		}
 		return true;
 	}
 	
-	static long findHorizontalReflection(String[] pattern) {
+	static long findHorizontalReflection(String[] pattern, int smug) {
 		for (int i = 0; i < pattern.length - 1; i++) {
-			if (isHorizontalReflected(pattern, i)) {
+			if (isHorizontalReflected(pattern, i, smug)) {
 				return (i + 1) * 100;
 			}
 		}
 		return -1;
 	}
 	
-	static boolean isHorizontalReflected(String[] pattern, int i) {
+	static boolean isHorizontalReflected(String[] pattern, int i, int smug) {
 		int top = i;
 		int bot = i + 1;
 		while (top >= 0 && bot < pattern.length) {
-			if (!pattern[top].equals(pattern[bot])) {
-				return false;
+			for (int j = 0; j < pattern[0].length(); j++) {
+				if (pattern[top].charAt(j) != pattern[bot].charAt(j)) {
+					smug--;
+					if (smug < 0) {
+						return false;
+					}
+				}
 			}
 			top--;
 			bot++;
+		}
+		if (smug != 0) {
+			return false;
 		}
 		return true;
 	}
